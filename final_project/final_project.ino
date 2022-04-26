77068b20451dc6cd65fc2dcb4a57fec551f69d60
Pierre (Shadow) <47398145+AiroPi@users.noreply.github.com>
2022-04-26 12:05:41 +0200

    Création d'une fonction
    Fonction pour définir les positions et valeurs par défaut au démarrage du programme.

--- final_project/final_project.ino
+++ final_project/final_project.ino
@@ -4,22 +4,53 @@
 
 // Définition des ports utilisés :
 #define CAPTOR_IF_POSITION_TRAY 3
-#define MOTOR_BM_RELAY 4
+#define RELAY_BM_MOTOR 4
 #define CAPTOR_BM_END_TURN 2
+#define FOUR_CHANNEL_RELAY_HBRIDGE 0x11;
 
+// define objects
 Multi_Channel_Relay HBridge;
-Pixy2 pixy;
+Pixy2 Pixy;
 
+// set up variables
+int Position;
 
 void setup() {
   Serial.begin(115200);
   Serial.print("Starting...\n");
 
+  // Set pins modes
+  pinMode(CAPTOR_IF_POSITION_TRAY, INPUT);
+  pinMode(RELAY_BM_MOTOR, OUTPUT);
+  pinMode(CAPTOR_BM_END_TURN, INPUT);
+
   // Setup pixy
-    pixy.init();
+    Pixy.init();
+
+  // Setup HBridge
+    HBridge.begin(0x11);
+
+  initProject();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+void initProject() {
+  /*
+  Function to initialize all default values of the project
+  */
+
+  // Stop the tray motor
+  HBridge.channelCtrl(0b0000);
+
+  // Turn off the light
+  Pixy.setLamp(LOW, LOW);
+
+  // Set BM motor initial position
+  digitalWrite(RELAY_BM_MOTOR, LOW); // Front descendant pour alimenter le moteur
+  while (digitalRead(CAPTOR_BM_END_TURN) == LOW); // Attente capteur fin de tour
+  digitalWrite(RELAY_BM_MOTOR, HIGH); // Front montant pour couper le moteur
 }
